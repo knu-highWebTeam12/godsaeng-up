db5cf048a116268b63de79ccd4e9550bfe2c04af
윤민수 <koelysa5696@naver.com>
2026-05-17 13:40:33 +0900

    chore: 스프링 프로젝트 초기 설정

---
+++ src/main/java/com/godsaeng/godsaeng_raid/GodsaengRaidApplication.java
@@ -0,0 +1,13 @@
+package com.godsaeng.godsaeng_raid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodsaengRaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodsaengRaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godsaeng/godsaeng_raid/GodsaengRaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godsaeng.godsaeng_raid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodsaengRaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
