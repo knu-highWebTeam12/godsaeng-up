19a1b238b7c13600a72989a50fe9ac33f57402d3
윤민수 <koelysa5696@naver.com>
2026-05-18 17:00:49 +0900

    feat: 공통 BaseEntity 추가 (생성/수정 시간 자동화)

---
+++ src/main/java/com/godsaeng/godsaeng_up/global/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.godsaeng.godsaeng_up.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+}
