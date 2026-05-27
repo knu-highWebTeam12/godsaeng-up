3d17f8f5e71b52a3f3f70c54bae674a22becec8d
김나영 <cmrny1230@gmail.com>
2026-05-27 17:35:39 +0900

    feat: mission repository 추가 (#12)

---
+++ src/main/java/com/godsaeng/godsaeng_up/mission/repository/MissionRepository.java
@@ -0,0 +1,19 @@
+package com.godsaeng.godsaeng_up.mission.repository;
+
+import com.godsaeng.godsaeng_up.mission.entity.Difficulty;
+import com.godsaeng.godsaeng_up.mission.entity.MissionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface MissionRepository extends JpaRepository<MissionEntity, Long> {
+
+    // 오늘 미션 전체 조회
+    List<MissionEntity> findByMemberIdAndMissionDate(Long memberId, LocalDate date);
+
+    // 중복 등록 방지용
+    Optional<MissionEntity> findByMemberIdAndMissionDateAndDifficulty(
+            Long memberId, LocalDate date, Difficulty difficulty);
+}
