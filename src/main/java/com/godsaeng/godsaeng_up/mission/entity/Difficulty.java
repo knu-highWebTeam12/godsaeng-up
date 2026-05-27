6fb91c14d2a28332d1595b70941676a1b0b9a789
김나영 <cmrny1230@gmail.com>
2026-05-27 14:36:16 +0900

    feat: mission entity 수정

--- src/main/java/com/godsaeng/godsaeng_up/mission/entity/Difficulty.java
+++ src/main/java/com/godsaeng/godsaeng_up/mission/entity/Difficulty.java
@@ -1,13 +1,13 @@
 package com.godsaeng.godsaeng_up.mission.entity;
 
 public enum Difficulty {
-    하, 중, 상;
+    EASY, NORMAL, HARD;
 
     public int getExp() {
         return switch (this) {
-            case 하 -> 10;
-            case 중 -> 30;
-            case 상 -> 50;
+            case EASY -> 10;
+            case NORMAL -> 30;
+            case HARD -> 50;
         };
     }
 }
