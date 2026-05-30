d196969959777c529de1fa93549c5e9ff8ece450
김나영 <cmrny1230@gmail.com>
2026-05-30 19:14:35 +0900

    fix: 미션 등록하기 화면 수정 (#12)

--- src/main/java/com/godsaeng/godsaeng_up/domain/mission/dto/MissionRequestDto.java
+++ src/main/java/com/godsaeng/godsaeng_up/domain/mission/dto/MissionRequestDto.java
@@ -3,11 +3,14 @@ package com.godsaeng.godsaeng_up.domain.mission.dto;
 import com.godsaeng.godsaeng_up.domain.mission.entity.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 @NoArgsConstructor
+
 public class MissionRequestDto {
     private String content;
     private Difficulty difficulty;
